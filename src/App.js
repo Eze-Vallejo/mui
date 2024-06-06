0f3bfe0bf4982062577bd3fd14a41e584460fd74
Ezequiel Vallejo <ezequiel.vallejo@globant.com>
2024-06-06 14:58:37 -0300

    added roboto font,added paper component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import TourCard from "./components/TourCard";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <TourCard />
+    </div>
+  );
 }
 
 export default App;
