1c19414872a8ebac31766debb1959feade656049
Ezequiel Vallejo <ezequiel.vallejo@globant.com>
2024-06-06 17:15:35 -0300

    added icons,image in tour card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import TourCard from "./components/TourCard";
 
 function App() {
   return (
     <div className="App">
-      <TourCard />
+      <Container>
+        <Grid container spacing={2}>
+          <TourCard />
+          <TourCard />
+          <TourCard />
+          <TourCard />
+        </Grid>
+      </Container>
     </div>
   );
 }
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,7 +1,56 @@
 import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Typography, Box, Rating } from "@mui/material";
+import { AccessTime } from "@mui/icons-material";
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  components: {},
+});
 
 const TourCard = () => {
-  return <Paper elevation={3}>TourCard</Paper>;
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/ab/3Falls_Niagara.jpg"
+          alt="niagara falls"
+          className="img"
+        />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            Immerse into the falls
+          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <AccessTime sx={{ width: 12.5 }} />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              5 hours
+            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+            <Rating
+              name="read-only"
+              value={4.5}
+              readOnly
+              precision={0.5}
+              size="small"
+            />
+            <Typography variant="body2" component="p" marginLeft={0.5}>
+              4.5
+            </Typography>
+            <Typography variant="body3" component="p" marginLeft={1.5}>
+              (655 reviews)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" component="h3" marginLeft={0}>
+              From C $187
+            </Typography>
+          </Box>
+        </Box>
+      </Paper>
+    </Grid>
+  );
 };
 
 export default TourCard;
